74e42ab8280e8561b2da8c464f0179be7db85983
unknown <harun>
2023-09-13 23:53:30 +0530

    phone and OTP frontend done fo today

--- lib/otp.dart
+++ lib/otp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
 
 class MyOtp extends StatefulWidget {
   const MyOtp({super.key});
@@ -10,6 +11,100 @@ class MyOtp extends StatefulWidget {
 class _MyOtpState extends State<MyOtp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final defaultPinTheme = PinTheme(
+      width: 56,
+      height: 56,
+      textStyle: TextStyle(
+          fontSize: 20,
+          color: Color.fromRGBO(30, 60, 87, 1),
+          fontWeight: FontWeight.w600),
+      decoration: BoxDecoration(
+        border: Border.all(color: Color.fromRGBO(234, 239, 243, 1)),
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
+
+    final focusedPinTheme = defaultPinTheme.copyDecorationWith(
+      border: Border.all(color: Color.fromRGBO(114, 178, 238, 1)),
+      borderRadius: BorderRadius.circular(8),
+    );
+
+    final submittedPinTheme = defaultPinTheme.copyWith(
+      decoration: defaultPinTheme.decoration?.copyWith(
+        color: Color.fromRGBO(234, 239, 243, 1),
+      ),
+    );
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.amber[700],
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back)),
+        ),
+        body: Container(
+          margin: EdgeInsets.only(left: 25, right: 25),
+          alignment: Alignment.center,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/dudehiking.png', width: 150, height: 150),
+                SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  'Register Mobile Number',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Verify OTP sent to your phone',
+                  style: TextStyle(fontSize: 14),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Pinput(
+                  length: 6,
+                  pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
+                  showCursor: true,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                    height: 50,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Verify OTP'),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.yellow[800],
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    )),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pushNamedAndRemoveUntil(
+                              context, 'phone', (route) => false);
+                        },
+                        child: Text(
+                          'Edit Phone Number?',
+                          style: TextStyle(color: Colors.black),
+                        ))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
